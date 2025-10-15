445ce296b05e838c6dbf8e8c1bc1f9ea012f37d1
Yeo Yu Xuan <yeoyuxuan302@gmail.com>
2025-10-15 11:28:25 +0800

    Updated overall dashboard layout (version1.0), including a blurred background and repositioned all charts neatly within a centered white block

--- W10Homework.js
+++ W10Homework.js
@@ -1,9 +1,35 @@
+// week 9 map
 var vg_1 = "W9Homework.json";
+vegaEmbed("#map", vg_1, { actions: false })
+  .then(function(result) {})
+  .catch(console.error);
 
-vegaEmbed("#map", vg_1).then(function(result) {
-}).catch(console.error);
-
+// week 10 bar chart
 var vg_2 = "W10Homework.json";
+vegaEmbed("#barchart", vg_2, { actions: false })
+  .then(function(result) {})
+  .catch(console.error);
 
-vegaEmbed("#barchart", vg_2).then(function(result) {
-}).catch(console.error);
+// gender pie chart
+var vg_3 = "GenderDonut.json";
+vegaEmbed("#genderdonut", vg_3, { actions: false })
+  .then(function(result) {})
+  .catch(console.error);
+
+// stratum pie chart
+var vg_4 = "StratumDonut.json";
+vegaEmbed("#stratumdonut", vg_4, { actions: false })
+  .then(function(result) {})
+  .catch(console.error);
+
+// duration stacked area chart
+var vg_5 = "DurationStackedArea.json";
+vegaEmbed("#durationarea", vg_5, { actions: false })
+  .then(function(result) {})
+  .catch(console.error);
+
+// slope chart
+var vg_6 = "SlopeChart.json";
+vegaEmbed("#slopechart", vg_6, { actions: false })
+  .then(function(result) {})
+  .catch(console.error);
